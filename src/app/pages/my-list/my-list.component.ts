4eb523f3c3f947b1b34f4d191e10d0e764a28ae6
Mukhit Home <vinniedrew7@gmail.com>
2023-12-27 05:37:40 +0600

    feat(pages): myfavs list

--- src/app/pages/my-list/my-list.component.ts
+++ src/app/pages/my-list/my-list.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { MovieService } from 'src/app/services/movies/movie.service';
 
 @Component({
     selector: 'app-my-list',
     templateUrl: './my-list.component.html',
     styleUrls: ['./my-list.component.css'],
 })
-export class MyListComponent {}
+export class MyListComponent {
+    favorites: any[] = [];
+    constructor(private movieService: MovieService) {}
+
+    ngOnInit() {
+        this.fetchFavoriteMovies();
+    }
+
+    fetchFavoriteMovies() {
+        this.movieService.getMyFavoriteMovies().then((res) => {
+            this.favorites = res;
+        });
+    }
+
+    removeFromMyList(id: number) {
+        const author = JSON.parse(localStorage.getItem('user') || '')?.name || 'dimash1234';
+
+        this.movieService.unfavMovie({ id, userName: author }).then(() => this.fetchFavoriteMovies());
+    }
+}
